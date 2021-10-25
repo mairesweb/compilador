8801a07dc77529b15ec1beb0cf5a7591efa0f52c
Marcelo Aires <mairesweb@gmail.com>
2021-10-25 20:39:31 -0300

    Alterado para funcionar o anal. sintático.

--- Principal.java
+++ Principal.java
@@ -1,16 +1,22 @@
 import lexico.NossoScanner;
 import lexico.Token;
+import sintatico.Parser;
 
 public class Principal {
   public static void main(String[] args) {
     NossoScanner nS = new NossoScanner("programa.xeque");
-    Token token = null;
+    Parser parser = new Parser(nS);
+
+    parser.expressao();
+
+    System.out.println("Compilador finalizado!");
+    //Token token = null;
     
-    do {
+    /*do {
       token = nS.lerToken();
       if (token != null) {
         System.out.println(token.mostrarInfo());
       }
-    } while (token != null);
+    } while (token != null);*/
   }
 }
